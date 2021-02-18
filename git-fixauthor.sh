554ccb80ce16826d8cfa3fa3f1db48c08e6b0e37
Tobias Kienzler <zommuter@users.noreply.github.com>
2021-02-18 10:42:22 +0100

    git-fixauthor.sh

--- git-fixauthor.sh
+++ git-fixauthor.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 git filter-branch --env-filter '
-OLD_EMAIL="old@email"
+#OLD_EMAIL="old@email"
 CORRECT_NAME="Tobias Kienzler"
 CORRECT_EMAIL="zommuter@users.noreply.github.com"
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
-then
+#if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+#then
     export GIT_COMMITTER_NAME="$CORRECT_NAME"
     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-fi
-if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
-then
+#fi
+#if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+#then
     export GIT_AUTHOR_NAME="$CORRECT_NAME"
     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
-fi
+#fi
 ' --tag-name-filter cat -- --branches --tags
